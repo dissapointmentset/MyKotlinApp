a4d9ef274e0126e65a4f386d04972797b18221ae
dissapointmentset <panins895@gmail.com>
2026-06-13 05:31:52 +0700

    мур мур

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var helwrld = findViewById<TextView>(R.id.StartName)//var - слово для создания переменной
+        var ger: EditText =
+            findViewById(R.id.gerich)//тип перемнной указывается либо в фигурных скобках либо через двоеточие
+        var butt: Button = findViewById(R.id.startbutton)
+        butt.setOnClickListener {
+            var text = ger.text.toString()
+            if (text == "penis")//если ввелось слово пенис, выводим сообщение с помощью библиотеки тост
+                Toast.makeText(this, "Ооооо даааа", Toast.LENGTH_SHORT).show()
+            else//иначе меняем тест кнопки
+                helwrld.text = text
         }
     }
 }
