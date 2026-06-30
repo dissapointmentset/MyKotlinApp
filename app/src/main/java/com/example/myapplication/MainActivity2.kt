e839dde695c8c6da813fbb3ee530077bd0c83d2f
dissapointmentset <panins895@gmail.com>
2026-06-30 21:40:08 +0700

    Я хочу умереть. ыыыыаааа2

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -27,8 +27,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TriStateCheckbox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -40,6 +44,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -47,6 +52,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.flow.callbackFlow
+import androidx.compose.foundation.layout.Spacer
 
 
 class MainActivity2 : ComponentActivity() {
@@ -54,74 +60,160 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Box(modifier = Modifier.fillMaxSize().background(color = Color.LightGray)){
-                LazyColumn() {
-                    items(1){
-                        ListItem("Янки", Color.Black, 0.5f, "немытый")
-                        ListItem("Саньки", Color.Red,0.4f, "вонючий")
-                        ListItem("Кирила", Color.DarkGray,0.8f, "Несокрушимый")
-                        ListItem("Алёны", Color.Blue, 1f, "Гигантский")
-                    }
-                }
-                Box(modifier = Modifier.align(Alignment.BottomCenter).
-                fillMaxWidth().
-                padding(10.dp)) { Adding()}
+            Glavnaya()
+        }
+    }
+}
+
+data class Question(
+    val name:String){
+}
+
+@Composable
+fun Glavnaya(){
+    val questions = remember {
+        mutableStateListOf(
+            Question("Вопрос 1"),
+
+        )
+    }
+    Box(modifier = Modifier.fillMaxSize().background(color = Color.LightGray)){
+        LazyColumn(modifier = Modifier.fillMaxHeight(0.9f)) {
+            items(questions.size){ index ->
+                val question = questions[index]
+                ListItem(
+                    name = question.name
+                )
+            }
+        }
+        Box(modifier = Modifier.align(Alignment.BottomCenter).
+        fillMaxWidth().
+        padding(10.dp)) {
+            Adding {
+                questions.add(
+                    Question(
+                        name = "Bопрос ${questions.size + 1}"
+                    )
+                )
             }
         }
     }
 }
 
-//data class Question(
-//    val name:String,
-//    val color: Color, val length: Float, val descript: String){
-//}
-//
-//@Composable
-//fun Glavnaya(){
-//    val questions = remember {
-//        mutableStateListOf(
-//            Question("Янки", Color.Black, 0.5f, "немытый"),
-//            Question("Саньки", Color.Red, 0.4f, "вонючий"),
-//            Question("Кирила", Color.DarkGray, 0.8f, "Несокрушимый"),
-//            Question("Алёны", Color.Blue, 1f, "Гигантский")
-//        )
-//    }
-//}
+
 
 @Composable
-fun Adding(){
-    Button(modifier = Modifier.fillMaxWidth(), onClick = {},
+fun Adding(onClick: () -> Unit){
+    Button(modifier = Modifier.fillMaxWidth(),
+        onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = Color.Black,       // Цвет фона кнопки
             contentColor = Color.White,       // Цвет текста и иконок
             disabledContainerColor = Color.Gray, // Цвет фона, когда кнопка неактивна
             disabledContentColor = Color.Black   // Цвет текста, когда кнопка неактивна
         )
-    ) {Text(text = "+Добавить вопрос", style = TextStyle(color = Color.White, fontSize = 38.sp)) }
+    ) {Text(text = "+Добавить вопрос",
+        style = TextStyle(color = Color.White, fontSize = 38.sp)
+    ) }
 }
 
 @Composable
-private fun ListItem(name: String,cvet:Color, dlin: Float, desc: String){
+private fun ListItem(name: String){
+    var task by remember {mutableStateOf("")  }
+    var typik by remember {mutableStateOf("Один верный ответ") }
     var expnd by remember { mutableStateOf(false) }
-    var dln = remember{mutableStateOf(dlin)}
-    Card(modifier = Modifier.fillMaxWidth(dln.value).
-    padding(20.dp).
-    shadow(5.dp).
-    clickable{dln.value+=0.01f
-        Log.d("MyLog","Пенис ${name} увеличели")}.pointerInput(Unit){
-        detectHorizontalDragGestures { change, dragAmount ->
-            Log.d("MyLog","Пенис ${name} почухан") }},
+    var state by remember { mutableStateOf(ToggleableState.Indeterminate) }
+    Card(modifier = Modifier.fillMaxWidth().
+        padding(20.dp).
+        shadow(5.dp),
         shape = RoundedCornerShape(15.dp)
     ) {
-        Column (modifier = Modifier.fillMaxSize()
-            .background(color = cvet)
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
             .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center) {
-            Text(modifier = Modifier.clickable {expnd=!expnd},
-                maxLines = if(expnd) 2 else 3,
-                text = "penis\n${name}\n(${desc})",
-                style = TextStyle(color = Color.Cyan, fontSize = 22.sp)) }
 
+            TextField(
+                value = task,
+                onValueChange = { task = it },
+                label = { Text(name) },
+                colors = TextFieldDefaults.colors(
+
+                    focusedTextColor = Color.Black,
+                    unfocusedTextColor = Color.DarkGray,
+
+                    // Цвет фона (заливки) поля
+                    focusedContainerColor = Color.White,
+                    unfocusedContainerColor = Color.LightGray,
+
+                    // Цвет нижней линии (индикатора)
+                    focusedIndicatorColor = Color.Blue,
+                    unfocusedIndicatorColor = Color.Gray,
+
+                    // Цвет курсора
+                    cursorColor = Color.Red,
+
+                    // Цвет подписи (лейбла)
+                    focusedLabelColor = Color.Blue,
+                    unfocusedLabelColor = Color.Gray,),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically) {
+
+                Text(
+                    text = typik,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Spacer(Modifier.weight(1f))
+
+                TriStateCheckbox(
+                    state = state,
+                    onClick = {
+                        // Переключаем между On и Off при клике
+                        state = if (state == ToggleableState.On)
+                        {ToggleableState.Indeterminate}
+                        else if (state == ToggleableState.Indeterminate)
+                        {ToggleableState.Off}
+                        else ToggleableState.On
+
+                        typik = if (state == ToggleableState.On) "Один верный ответ"
+                        else if (state == ToggleableState.Indeterminate)
+                        "Несколько верных ответов"
+                        else "Ответ необходимо написать"
+                    }
+                )
+                TextField(
+                    value = task,
+                    onValueChange = { task = it },
+                    label = { Text("введите верный ответ") },
+                    colors = TextFieldDefaults.colors(
+
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Color.DarkGray,
+
+                        // Цвет фона (заливки) поля
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.LightGray,
+
+                        // Цвет нижней линии (индикатора)
+                        focusedIndicatorColor = Color.Blue,
+                        unfocusedIndicatorColor = Color.Gray,
+
+                        // Цвет курсора
+                        cursorColor = Color.Red,
+
+                        // Цвет подписи (лейбла)
+                        focusedLabelColor = Color.Blue,
+                        unfocusedLabelColor = Color.Gray,),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
